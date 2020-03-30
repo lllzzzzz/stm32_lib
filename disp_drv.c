0d88a8e32f989c1e5862a99da4ac3b58173979bb
lllzzzzz <450697562@qq.com>
2020-03-30 19:49:16 +0800

    进一步完善屏幕

--- disp_drv.c
+++ disp_drv.c
@@ -3,54 +3,73 @@
 #include "ltdc.h"
 #include "dma2d.h"
 
-const struct screen_info sreen_akt = 
-{
-  .ID = 0X4342,
-  .width = 480,
-  .height = 272,
+const struct screen_info sreen_atk =
+    {
+        .ID = 0X4342,
+        .width = 480,
+        .height = 272,
+        .pixsize = 2,
 };
 
 /**
-  * @brief  Initial sdram
-  * @note   ltdc_fill
+  * @brief  fill_rect
+  * @note   fill_rect
   * @param  hsdram : SDRAM handle 
   * @retval void
   */
-void LTDC_Fill(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint32_t color)
+void fill_rect(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint32_t color)
 {
-  uint16_t c_x0, c_y0, c_x1, c_y1; //以LCD面板为基准的坐标系,不随横竖屏变化而变化
+  uint16_t c_x0, c_y0, c_x1, c_y1;
 
-  //坐标系转换
+  /* Coordinate transformation */
   c_x0 = y0;
-  c_y0 = sreen_akt.height - x1 - 1;
+  c_y0 = sreen_atk.height - x1 - 1;
   c_x1 = y1;
-  c_y1 = sreen_akt.height - x0 - 1;
- 
-  __HAL_RCC_DMA2D_CLK_ENABLE();   //使能DM2D时钟
-  // DMA2D->CR &= ~(DMA2D_CR_START); //先停止DMA2D
-  // DMA2D->CR = DMA2D_R2M;          //寄存器到存储器模式
-  // DMA2D->OPFCCR = 0X02;           //设置颜色格式
-  // DMA2D->OOR = offline;           //设置行偏移
+  c_y1 = sreen_atk.height - x0 - 1;
 
-  // DMA2D->OMAR = (0xc0000000 + 2 * (sreen_akt.width * c_y0 + c_x0));                                     //输出存储器地址
-  // DMA2D->NLR = (c_y1 - c_y0 + 1) | ((c_x1 - c_x0 + 1) << 16); //设定行数寄存器
-  // DMA2D->OCOLR = color;                                   //设定输出颜色寄存器
-  // DMA2D->CR |= DMA2D_CR_START;                            //启动DMA2D
-  //   uint32_t time_out = 0;
-  // while ((DMA2D->ISR & (DMA2D_FLAG_TC)) == 0)             //等待传输完成
-  // {
-  //   time_out++;
-  //   if (time_out > 0X1FFFFF)
-  //     break; //超时退出
-  // }
-  // DMA2D->IFCR |= DMA2D_FLAG_TC; //清除传输完成标志
-  /* DMA config */
-  
-  uint32_t dst_address = (uint32_t)(0xc0000000 + 2 * (sreen_akt.width * c_y0 + c_x0));
+  /* DMA configure */
+  uint16_t offset = sreen_atk.width - (c_x1 - c_x0 + 1);
+  uint32_t dst_address = (uint32_t)(0xc0000000 + sreen_atk.pixsize * (sreen_atk.width * c_y0 + c_x0));
 
-  HAL_DMA2D_ConfigLayer(&hdma2d, 1);
-  HAL_DMA2D_Start(&hdma2d, color, dst_address, c_x1 - c_x0 + 1, c_y1 - c_y0 + 1);
-  HAL_DMA2D_PollForTransfer(&hdma2d, 100);
+  hdma2d.Init.OutputOffset = offset;
+  if (HAL_DMA2D_Init(&hdma2d) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_DMA2D_ConfigLayer(&hdma2d, 1) != HAL_OK)
+    ;
+  {
+    Error_Handler();
+  }
+  if (HAL_DMA2D_Start(&hdma2d, color, dst_address, c_x1 - c_x0 + 1, c_y1 - c_y0 + 1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* DMA transfer */
+  if (HAL_DMA2D_PollForTransfer(&hdma2d, 100) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
 
+/**
+  * @brief  write_point
+  * @note   fill_rect
+  * @param  hsdram : SDRAM handle 
+  * @retval void
+  */
+void write_point(uint16_t x, uint16_t y, uint32_t color)
+{
+  *(uint16_t *)((uint32_t)0xc0000000 + sreen_atk.pixsize * (sreen_atk.width * (sreen_atk.height - x - 1) + y)) = color;
+}
 
+/**
+  * @brief  drow_point
+  * @note   fill_rect
+  * @param  hsdram : SDRAM handle 
+  * @retval void
+  */
+uint32_t read_point(uint16_t x, uint16_t y)
+{
+  return *(uint16_t *)((uint32_t)0xc0000000 + sreen_atk.pixsize * (sreen_atk.width * (sreen_atk.height - x - 1) + y));
 }
--- disp_drv.h
+++ disp_drv.h
@@ -8,8 +8,11 @@ struct screen_info
   uint16_t ID;
   uint16_t width;
   uint16_t height;
+  uint8_t pixsize;
 };
 
-void LTDC_Fill(uint16_t , uint16_t , uint16_t , uint16_t , uint32_t );
+void fill_rect(uint16_t , uint16_t , uint16_t , uint16_t , uint32_t );
+void write_point(uint16_t x, uint16_t y, uint32_t color);
+uint32_t read_point(uint16_t x, uint16_t y);
 
 #endif
